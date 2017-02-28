ffd6dac40bd8ccbb040142aa19d34fb8bcb65af1
Kenfajri Nugraha <kenfajri11@gmail.com>
2017-02-28 17:56:20 +0700

    Add files via upload

---
+++ Minggu-2/Mahasiswa.java
@@ -0,0 +1,7 @@
+class Mahasiswa {
+	public static void main(String argv[]) {
+		System.out.println("Nama Lengkap : Kenfajri Nugraha");
+		System.out.println("NIM          : 10115076");
+		System.out.println("Alamat       : Bandung");
+	}
+}
