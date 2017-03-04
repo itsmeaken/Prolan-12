3400ee0eb5c8cfd0722d50f7844079755abce251
Kenfajri Nugraha <kenfajri11@gmail.com>
2017-03-04 10:01:10 +0700

    Add files via upload

---
+++ Minggu-2/InputOutput2.java
@@ -0,0 +1,5 @@
+class InputOutput2 {
+	public static void main(String argv[]){
+		System.out.print("Belajar \nJava \nYuks");
+	}
+}
---
+++ Minggu-2/InputOutput3.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+class InputOutput3 {
+	public static void main(String argv[]){
+	Scanner buatnama = new Scanner (System.in);
+	String nama;
+	nama = buatnama.nextLine();
+	}
+}
